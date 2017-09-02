0a835b59c3250534a9d8e0e0740490eeca07c45c
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-09-02 15:54:52 +0200

    Fix uploading via form and API

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(plugin)]
+#![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
@@ -7,12 +7,16 @@ extern crate rand;
 extern crate tera;
 
 use std::io;
+use std::io::{Write, Read};
 use std::path::{Path, PathBuf};
 use std::fs::File;
+use std::borrow::Borrow;
+use std::error::Error;
 
 use rocket::{Data, State};
 use rocket::fairing::AdHoc;
-use rocket::response::NamedFile;
+use rocket::response::{NamedFile, Redirect};
+use rocket::request::Form;
 use rocket_contrib::Template;
 
 use tera::Context;
@@ -23,21 +27,59 @@ use paste_id::{PasteID, ID_LEN};
 struct PublicUrl(String);
 const DEFAULT_PUBLIC_URL: &'static str = "http://localhost:8000";
 
+
+#[derive(Debug, FromForm)]
+struct PasteForm {
+    #[form(field = "code")]
+    text: String
+}
+
 #[get("/")]
 fn index() -> Template {
     let context = Context::new();
     Template::render("index", &context)
 }
 
-#[post("/", data = "<paste>")]
-fn upload(paste: Data, public_url: State<PublicUrl>) -> io::Result<String> {
+// Save a string as a paste and return the URL
+fn upload_to_file(paste: String, public_url: State<PublicUrl>) -> io::Result<String> {
     let id = PasteID::new(ID_LEN);
-    let filename = format!("upload/{id}", id = id);
-    let url = format!("{public_url:?}/{id}\n", public_url = public_url.0, id = id);
 
-    // Write the paste out to the file and return the URL.
-    paste.stream_to_file(Path::new(&filename))?;
-    Ok(url)
+    let path_name = format!("upload/{id}", id = id);
+    let path = Path::new(&path_name);
+    let display = path.display();
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}",
+                           display,
+                           why.description()),
+        Ok(file) => file,
+    };
+    match file.write_all(paste.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", 
+                           display,
+                           why.description()),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+
+    Ok(format!("{public_url}/{id}\n", public_url = public_url.0, id = id))
+}
+
+#[post("/api", data = "<paste>")]
+fn upload_plain(paste: Data, public_url: State<PublicUrl>) -> io::Result<String> {
+    let mut buffer = String::new();
+    paste.open().read_to_string(&mut buffer)?;
+    Ok(upload_to_file(buffer, public_url).unwrap())
+}
+
+#[post("/", data = "<paste>")]
+fn upload_html(paste: Result<Form<PasteForm>, Option<String>>, public_url: State<PublicUrl>) -> Redirect {
+    match paste {
+        Ok(f) => {
+            let paste_text = f.get().text.clone();
+            Redirect::to(upload_to_file(paste_text, public_url).unwrap().borrow())
+        },
+        Err(Some(_)) => Redirect::to("/404"),
+        Err(None) => Redirect::to("/404"),
+    }
 }
 
 #[get("/<id>")]
@@ -51,10 +93,15 @@ fn static_files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
-
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/", routes![static_files, index, upload, retrieve])
+        .mount("/", routes![
+            static_files,
+            index,
+            upload_plain,
+            upload_html,
+            retrieve
+        ])
         .attach(AdHoc::on_attach(|rocket| {
             println!("Adding public_url managed state...");
             let public_url = PublicUrl(String::from(
